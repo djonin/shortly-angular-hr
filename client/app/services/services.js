a524b83c056c67619aa9077a83a913fac15dafd6
Shyam Sunder Goyal <shyamsunder.goyal@autodesk.com>
2015-08-03 14:05:34 -0700

    Passed the test cases

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,7 +1,31 @@
 angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
-  // Your code here
+  var links = function() {
+    return $http({
+      method: 'GET',
+      url: '/api/links'
+      })
+      .then(function (resp) {
+        return resp.data;
+      });
+  };
+
+  var newLink = function(link) {
+    return $http({
+      method: 'POST',
+      url: '/api/links',
+      data : link
+      })
+      .then(function (resp) {
+        return resp.status === 201;
+      });
+  };
+
+  return {
+    links: links,
+    newLink : newLink
+  };
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
